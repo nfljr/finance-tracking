b1b709cd2b6b7539d6712bdc4382b5a32512b466
Puja Naufal <nfljr14@gmail.com>
2026-03-14 22:26:50 +0700

    malam ini besok libur

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, } from "recharts";
 
 export default function Dashboard() {
   const [transactions, setTransactions] = useState(() => {
@@ -33,6 +34,8 @@ export default function Dashboard() {
   const [type, setType] = useState("expense");
   const [category, setCategory] = useState("Food");
   const [date, setDate] = useState("");
+  const [search, setSearch] = useState("");
+  const [filterType, setFilterType] = useState("all");
 
   useEffect(() => {
     localStorage.setItem("transactions", JSON.stringify(transactions));
@@ -80,10 +83,79 @@ export default function Dashboard() {
 
   const balance = totalIncome - totalExpense;
 
+  const expenseByCategory = {};
+
+  transactions
+    .filter((t) => t.type === "expense")
+    .forEach((t) => {
+      if (!expenseByCategory[t.category]) {
+        expenseByCategory[t.category] = 0;
+      }
+
+      expenseByCategory[t.category] += t.amount;
+    });
+
+  const chartData = Object.keys(expenseByCategory).map((key) => ({
+    name: key,
+    value: expenseByCategory[key],
+  }));
+
+  const COLORS = [
+    "#3B82F6",
+    "#EF4444",
+    "#22C55E",
+    "#F59E0B",
+    "#8B5CF6",
+    "#14B8A6",
+  ];
+
+  const filteredTransaction =transactions.filter((transactions) => {
+    const keyword = search.toLowerCase();
+
+    const matchSearch = 
+    transactions.title.toLowerCase().includes(keyword) ||
+    transactions.category.toLowerCase().includes(keyword);
+
+    const matchType =
+    filterType === "all" || transactions.type === filterType;
+
+    return matchSearch && matchType;
+  });
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold mb-6">Personal Finance Tracker</h1>
+      <div className="bg-white p-4 rounded-lg shadow mb-6">
+            <h2 className="text-xl font-semibold mb-4">
+              Expense by Category
+            </h2>
 
+            {chartData.length === 0 ? (
+              <p className="text-gray-500">Belum ada data expense</p>
+            ) : (
+              <ResponsiveContainer width="100%" height={250}>
+                  <PieChart>
+                    <Pie
+                      data={chartData}
+                      dataKey="value"
+                      nameKey="name"
+                      outerRadius={100}
+                      label = {({name, percent}) => 
+                        `${name} ${(percent * 100).toFixed(0)}%`
+                      }
+                    >
+                      {chartData.map((entry, index) => (
+                        <cell
+                          key={index}
+                          fill={COLORS[index % COLORS.length]}
+                        />
+                      ))}
+                    </Pie>
+                  </PieChart>
+              </ResponsiveContainer>
+            )}
+        </div>
+        
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         <div className="bg-green-500 text-white p-4 rounded-lg shadow">
             <h2 className="text-lg font-semibold">Total Income</h2>
@@ -163,11 +235,29 @@ export default function Dashboard() {
       <div className="bg-white p-4 rounded-lg shadow">
         <h2 className="text-xl font-semibold mb-4">Transaction List</h2>
 
-        {transactions.length === 0 ? (
-          <p className="text-gray-500">Belum ada transaksi.</p>
+        <input 
+          type="text" 
+          placeholder="search by title or category..."
+          className="border p-2 rounded 2-full mb-4"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <select
+          className="border p-2 rounded w-full mb-4"
+          value={filterType}
+          onChange={(e) => setFilterType(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="income">Income</option>
+          <option value="expense">Expense</option>
+        </select>
+
+        {filteredTransaction.length === 0 ? (
+          <p className="text-gray-500">Transaksi Tidak ditemukan.</p>
         ) : (
           <div className="flex flex-col gap-3">
-            {transactions.map((transaction) => (
+            {filteredTransaction.map((transaction) => (
               <div
                 key={transaction.id}
                 className="border rounded p-3 flex justify-between items-center"
